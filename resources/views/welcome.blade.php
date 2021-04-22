85fb9d1087651fae1bdf375f2f23f02443781e55
nebstarmalala <nebstartrizzle@gmail.com>
2021-04-23 00:16:23 +0300

    add image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     <div class="content">
+        <br>
+        <img src="/img/blog_img.jpg" alt="Blog Logo" height="400">
         <div class="title m-b-md">
            BLOGS
         </div>
