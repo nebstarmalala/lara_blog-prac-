b8c942efad1a5b522a6f833060407c7aaf2c7d48
nebstarmalala <nebstartrizzle@gmail.com>
2021-04-23 01:16:07 +0300

    redesign home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,12 @@
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     <div class="content">
-        <br>
-        <img src="/img/blog_img.jpg" alt="Blog Logo" height="400">
+        
         <div class="title m-b-md">
-           By {{ $name}} ({{ $occupation}})
+            HOME PAGE
         </div>
         <br>
-        @foreach($blogs as $blog)
-            <p>{{ $blog['author']}} -> {{ $blog['title'] }}</p>
-        @endforeach
+        
     </div>
 </div>
 @endsection
-
