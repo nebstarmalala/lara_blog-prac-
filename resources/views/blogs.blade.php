13c71e32606fed2fe4c15a88be492906f80afebb
nebstarmalala <nebstartrizzle@gmail.com>
2021-04-22 22:22:16 +0300

    initial commit

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="content">
+        <div class="title m-b-md">
+           BLOGS
+        </div>
+        <br>
+        @foreach($blogs as $blog)
+            <p>{{ $blog['author']}} -> {{ $blog['title'] }}</p>
+        @endforeach
+    </div>
+</div>
+@endsection
+        
+    
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/blogs', function () {
+    $blogs = [
+        ["title" => "Laravel for begginers", "author" => "Nebstar Malash"],
+        ["title" => "PHP for intermediate", "author" => "Shem ian"],
+        ["title" => "Python for Professionals", "author" => "Martin Mato"],
+        ["title" => "Ruby for hackers", "author" => "Agusto wafula"],
+    ];
+
+    return view('blogs', ["blogs" => $blogs]);
+});
