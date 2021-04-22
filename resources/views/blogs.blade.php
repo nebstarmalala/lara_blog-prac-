54d7e51de5adff19442f3b69ab4427178c66514a
nebstarmalala <nebstartrizzle@gmail.com>
2021-04-23 01:15:35 +0300

    initial commit

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="content">
+        <br>
+        <img src="/img/blog_img.jpg" alt="Blog Logo" height="400">
+        <div class="title m-b-md">
+           By {{ $name}} ({{ $occupation}})
+        </div>
+        <br>
+        @foreach($blogs as $blog)
+            <p>{{ $blog['author']}} -> {{ $blog['title'] }}</p>
+        @endforeach
+    </div>
+</div>
+@endsection
