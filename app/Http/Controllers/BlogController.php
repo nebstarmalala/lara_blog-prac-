59e1018709c743dfda997652f61c717dabb78de7
nebstarmalala <nebstartrizzle@gmail.com>
2021-04-23 12:38:04 +0300

    add blog routes handler

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function index(){
+        $blogs = [
+            ["title" => "Laravel for begginers", "author" => "Nebstar Malash"],
+            ["title" => "PHP for intermediate", "author" => "Shem ian"],
+            ["title" => "Python for Professionals", "author" => "Martin Mato"],
+            ["title" => "Ruby for hackers", "author" => "Agusto wafula"],
+        ];
+    
+        $name = request('name');
+        $occupation = request('occupation');
+    
+        return view('blogs', [
+            "blogs" => $blogs,
+            'name' => $name,
+            'occupation' => $occupation
+            ]);
+    }
+
+    public function show($id){
+        return view('details', ['id' => $id]);
+    }
 }
