8454795e5c657ae3a02eaa0a1430236e37632e3d
nebstarmalala <nebstartrizzle@gmail.com>
2021-04-26 01:00:52 +0300

    handle show action

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,15 +1,9 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <div class="content">
-        <br>
-        <br>
-        <div class="content">
-            <div class="title m-b-md">
-                Blog List - {{$id }}
-            </div>
-        </div>
-    </div>
+<div class="wrapper blog-details">
+    <h2>{{ $blog->title }}</h2>
+    <p>{{$blog->body }}</p>
+    <p>By {{$blog->author}}</p>
 </div>
 @endsection
